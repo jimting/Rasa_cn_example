c0d9215a639c9f3f085ac0f001816e28333a1641
Jimting <handsome841206@gmail.com>
2019-12-15 23:22:21 +0800

    update some intent

--- actions/action.py
+++ actions/action.py
@@ -7,5 +7,18 @@ class ActionAskWeather(Action):
         return 'action_ask_weather'
 
     def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_message("您问的天气地点是哪里呢")
-        return [SlotSet("city", "深圳")]
+        city = next(tracker.get_latest_entity_values('city'), None)
+		searching_city = "none"
+		if city is not None:
+			searching_city = city
+		date = next(tracker.get_latest_entity_values('date'), None)
+		searching_date = "none"
+		if date is not None:
+			searching_date = date
+		data = {
+			"intent" : "action_ask_weather",
+			"city" : searching_city,
+			"date" : searching_date
+		}
+		dispatcher.utter_message(format(data))
+		return []
