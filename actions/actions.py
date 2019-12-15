9602a873e546436c504be72ed155d814eef48cba
Jimting <handsome841206@gmail.com>
2019-12-15 23:53:57 +0800

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -6,17 +6,17 @@ class ActionAskWeather(Action):
 
     def run(self, dispatcher, tracker, domain):
         city = next(tracker.get_latest_entity_values('city'), None)
-		searching_city = "none"
-		if city is not None:
-			searching_city = city
-		date = next(tracker.get_latest_entity_values('date'), None)
-		searching_date = "none"
-		if date is not None:
-			searching_date = date
-		data = {
-			"intent" : "action_ask_weather",
-			"city" : searching_city,
-			"date" : searching_date
-		}
-		dispatcher.utter_message(format(data))
-		return []
+        searching_city = "none"
+        if city is not None:
+            searching_city = city
+        date = next(tracker.get_latest_entity_values('date'), None)
+        searching_date = "none"
+        if date is not None:
+            searching_date = date
+        data = {
+            "intent" : "action_ask_weather",
+            "city" : searching_city,
+            "date" : searching_date
+        }
+        dispatcher.utter_message(format(data))
+        return []
