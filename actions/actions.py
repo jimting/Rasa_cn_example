9bae7908459d691a8d2c2a631f16ddaf5e6d47af
Jimting <handsome841206@gmail.com>
2019-12-18 14:11:58 +0800

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -6,5 +6,5 @@ class ActionAskWeather(Action):
 
     def run(self, dispatcher, tracker, domain):
         searching_city = next(tracker.get_latest_entity_values('city'), None)
-        dispatcher.utter_message(searching_city + "的天氣很好哦！")
+        dispatcher.utter_message(str(searching_city)+"的天氣很好哦！")
         return []
