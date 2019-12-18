92369d479c1737619fbfda4048bdea8937460ead
Jimting <handsome841206@gmail.com>
2019-12-18 14:05:22 +0800

    update the actions method

--- actions/actions.py
+++ actions/actions.py
@@ -1,10 +1,10 @@
 from rasa_core_sdk import Action
-from rasa_core_sdk.events import SlotSet
 
 class ActionAskWeather(Action):
     def name(self):
         return 'action_ask_weather'
 
     def run(self, dispatcher, tracker, domain):
-        ent = tracker.get_latest_entity_values('city')
-        return [SlotSet('city', ent)]
+        searching_city = next(tracker.get_latest_entity_values('city'), None)
+        dispatcher.utter_message(searching_city + "的天氣很好哦！")
+        return []
